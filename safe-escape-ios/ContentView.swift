e20fa6274ad65fc52d0dc0cfd7f96c991fc984a3
kaseul <dtrbu123@g.skku.edu>
2025-07-15 17:29:12 +0900

    🎉 [프로젝트] 프로젝트 생성

---
+++ safe-escape-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  safe-escape-ios
+//
+//  Created by kaseul on 7/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ safe-escape-ios/SafeEscapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeEscapeApp.swift
+//  safe-escape-ios
+//
+//  Created by kaseul on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct safe_escape_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

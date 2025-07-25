b8f827514b09cb2779ae3f39093d2ca5344649d6
kaseul <dtrbu123@g.skku.edu>
2025-07-25 17:14:38 +0900

    ✨ [공통] 주소 도메인 모델 추가

---
+++ safe-escape-ios/Geolocation/Domain/Model/Address.swift
@@ -0,0 +1,14 @@
+//
+//  Address.swift
+//  safe-escape-ios
+//
+//  Created by kaseul on 7/23/25.
+//
+
+import Foundation
+
+struct Address {
+    let road: String // 도로명 주소
+    let jibun: String? // 지번 주소
+    let coordinate: Coordinate? // 좌표
+}

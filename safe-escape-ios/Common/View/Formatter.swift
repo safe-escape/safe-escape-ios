b59d69c846dbd8720b1d52d46ae23313dfe690b3
kaseul <dtrbu123@g.skku.edu>
2025-07-25 17:40:30 +0900

    💄 [공통] 위치 관련 거리 / 시간 표시 Formatter 추가

---
+++ safe-escape-ios/Common/View/Formatter.swift
@@ -0,0 +1,58 @@
+//
+//  Formatter.swift
+//  safe-escape-ios
+//
+//  Created by kaseul on 7/24/25.
+//
+
+import Foundation
+
+// 공통 거리 표시 Formatter
+public enum DistanceFormatter {
+    
+    // 1km 미만은 m / 1km 이상은 km 단위로 소수점 이하 0 제외 첫째 자리까지 표시
+    public static func format(_ meters: Double) -> String {
+        if meters < 1000 {
+            return "\(Int(meters))m"
+        } else {
+            return String(format: "%.1fkm", meters / 1000).replacingOccurrences(of: ".0", with: "")
+        }
+    }
+    
+}
+
+// 공통 시간 표시 Formatter
+public enum TimeFormatter {
+    
+    // 초 단위 -> 00시간 00분으로 표시
+    public static func format(_ seconds: Int) -> String {
+        var seconds = seconds
+        
+        var hours = seconds / 3600
+        seconds %= 3600
+        
+        var minutes = seconds / 60
+        seconds %= 60
+        
+        if seconds > 0 {
+            if minutes == 59 {
+                hours += 1
+                minutes = 0
+            } else {
+                minutes += 1
+            }
+        }
+        
+        var result = ""
+        if hours > 0 {
+            result += "\(hours)시간 "
+        }
+        
+        if minutes > 0 {
+            result += "\(minutes)분"
+        }
+        
+        return result.trimmingCharacters(in: .whitespaces)
+    }
+    
+}
